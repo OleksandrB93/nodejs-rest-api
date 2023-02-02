94f52a3c5fc0899ade6fb49ed99dfb6d09eae38d
OleksandrB93 <barabashsan4ik@gmail.com>
2023-02-02 17:01:13 +0200

    created logout

--- controlers/auth/login.js
+++ controlers/auth/login.js
@@ -7,6 +7,7 @@ const { User } = require("../../models");
 const login = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
+
   if (!user || !user.comparePassword(password)) {
     throw new Unauthorized("Email or password is wrong");
   }
@@ -15,7 +16,7 @@ const login = async (req, res) => {
     id: user._id,
   };
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
-  
+  await User.findByIdAndUpdate(user._id, { token });
   res.json({
     token,
     user: {
@@ -24,6 +25,5 @@ const login = async (req, res) => {
     },
   });
 };
-
 
 module.exports = login;
