7e526be92624d9c4d02247680205855bd5be566f
OleksandrB93 <barabashsan4ik@gmail.com>
2023-02-06 15:42:53 +0200

    fix geBytId

--- controlers/contacts/getById.js
+++ controlers/contacts/getById.js
@@ -5,10 +5,11 @@ const { Contact } = require("../../models/contacts");
 
 const getById = async (req, res) => {
   const owner = req.user._id;
-  const id = req.params.contactId;
+  const id = ObjectId(req.params.contactId);
   console.log(id);
 
   const result = await Contact.findOne({ owner, id });
+
   console.log(id);
   if (!result) {
     throw NotFound(`Contact with id=${id} not found`);
