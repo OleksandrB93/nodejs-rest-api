30a085a55783636443dd81ab5a7f691181e3b344
OleksandrB93 <barabashsan4ik@gmail.com>
2023-02-17 15:30:10 +0200

    fix imageName

--- controlers/users/updateAvatar.js
+++ controlers/users/updateAvatar.js
@@ -1,24 +1,25 @@
 const path = require("path");
 const fs = require("fs/promises");
 const { User } = require("../../models");
+const { v4: uuidv4 } = require('uuid');
 
 const avatarDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
-    const { path: tempUpload, originalname } = req.file;
-    const { _id: id } = req.user;
-    const imageName = `${id}_${originalname}`;
-    try {
-        const resultUpload = path.join(avatarDir, imageName);  
-        await fs.rename(tempUpload, resultUpload); 
+  const { path: tempUpload, originalname } = req.file;
+  const { _id: id } = req.user;
+  const imageName = `${id}_${originalname}+${uuidv4()}`;
+  try {
+    const resultUpload = path.join(avatarDir, imageName);
+    await fs.rename(tempUpload, resultUpload);
 
-        const avatarURL = path.join("public", "avatars", imageName);
-        await User.findOneAndUpdate(req.user._id, { avatarURL });
-        res.json({avatarURL});
-    } catch (error) {
-        await fs.unlink(tempUpload); 
-        throw error;
-    }
-}
+    const avatarURL = path.join("public", "avatars", imageName);
+    await User.findOneAndUpdate(req.user._id, { avatarURL });
+    res.json({ avatarURL });
+  } catch (error) {
+    await fs.unlink(tempUpload);
+    throw error;
+  }
+};
 
 module.exports = updateAvatar;
